1b7c23b60e17e86b5c0f73ca6cdcb66101d9ba89
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-03-17 07:55:50 -0300

    aula 3 strings

---
+++ aula3.py
@@ -0,0 +1,11 @@
+print("Fala sobre strings")
+#simples
+print('simples')
+# strings 
+print(123456)
+# ultilizando aspas duplas e simples
+print("Com 'aspas'") # Melhor recomendação
+# escape
+print("E aspas \"duplas\"")
+# r
+print(r"com r mostrando \"aspas\"") # recomenda usar em expressões regulares

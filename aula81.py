e10f64999ea16cadf5c2e958ee844ffac14af4c1
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-05-23 14:10:04 -0300

    aula lambda

---
+++ aula81.py
@@ -0,0 +1,43 @@
+# Introdução à função lambda (função anônima de uma linha)
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única
+# expressão.
+# lista = [
+#     {'nome': 'Luiz', 'sobrenome': 'miranda'},
+#     {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+#     {'nome': 'Daniel', 'sobrenome': 'Silva'},
+#     {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+#     {'nome': 'Aline', 'sobrenome': 'Souza'},
+# ]
+# lista = [4, 32, 1, 34, 5, 6, 6, 21, ]
+# lista.sort(reverse=True)
+# sorted(lista)
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+    {'nome': 'bruno', 'sobrenome': 'Souz'}
+]
+#def mostrar(item):
+#    #print(item)
+#    return item['nome']
+
+#lista.sort(key=mostrar)
+#utilizando lambda
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+
+#lista.sort(key=lambda item: item['nome'])
+#com sorted
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)

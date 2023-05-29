0e0786d59b362ab5baa05036c1e9f91c7c66579b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-05-29 14:12:02 -0300

    aula modulos

---
+++ aula96.py
@@ -0,0 +1,30 @@
+# Módulos padrão do Python (import, from, as e *)
+# https://docs.python.org/3/py-modindex.html
+# inteiro - import nome_modulo
+# Vantagens: você tem o namespace do módulo
+# Desvantagens: nomes grandes
+
+import sys
+
+
+
+# partes - from nome_modulo import objeto1, objeto2
+# Vantagens: nomes pequenos
+# Desvantagens: Sem o namespace do módulo
+
+
+
+# alias 1 - import nome_modulo as apelido
+
+
+
+# alias 2 - from nome_modulo import objeto as apelido
+
+
+
+# Vantagens: você pode reservar nomes para seu código
+# Desvantagens: pode ficar fora do padrão da linguagem
+
+# má prática - from nome_modulo import *
+# Vantagens: importa tudo de um módulo
+# Desvantagens: importa tudo de um módulo

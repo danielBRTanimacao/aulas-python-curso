6e537954cb155557a46b841eeecefdb6b64f9ba8
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-06-25 14:06:01 -0300

    update aula polimorfismo

--- aula144.py
+++ aula144.py
@@ -13,3 +13,38 @@
 # por objetos de uma subclasse sem quebrar a aplicação.
 # Sobrecarga de métodos (overload)  🐍 = ❌
 # Sobreposição de métodos (override) 🐍 = ✅
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool: ...
+
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print("E-mail: Enviando...", self.mensagem)
+        return True
+    
+
+class NotificacaoSms(Notificacao):
+    def enviar(self) -> bool:
+        print("SMS: Enviando...", self.mensagem)
+        return False
+
+
+def notificar(notificar: Notificacao):
+    notificar_enviar = notificar.enviar()
+
+    if notificar_enviar:
+        print('A notificação foi enviada')
+    else:
+        print('A notificação não foi enviada')
+    
+
+notificacao_email = NotificacaoEmail('Enviou essa bomba kkk')
+notificar(notificacao_email)
+
+notificar(NotificacaoSms('Olha a mensagem kkk'))

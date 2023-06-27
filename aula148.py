bc60578a8641aa912d91561c6e4c39d83f417356
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-06-27 13:06:54 -0300

    update aula dunder __new__

---
+++ aula148.py
@@ -0,0 +1,8 @@
+# __new__ e __init__ em classes Python
+# __new__ é o método responsável por criar e
+# retornar o novo objeto. Por isso, new recebe cls.
+# __new__ ❗️DEVE retornar o novo objeto❗️
+# __init__ é o método responsável por inicializar
+# a instância. Por isso, init recebe self.
+# __init__ ❗️NÃO DEVE retornar nada (None)❗️
+# object é a super classe de uma classe

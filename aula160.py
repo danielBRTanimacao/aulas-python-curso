32c55033981575539cd8e46c3bead23644b23de7
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-07-15 13:43:39 -0300

    aula namedtuples

---
+++ aula160.py
@@ -0,0 +1,9 @@
+# namedtuples - tuplas imutáveis com nomes para valores
+# Usamos namedtuples para criar classes de objetos que são apenas um
+# agrupamento de atributos, como classes normais sem métodos, ou registros de
+# bases de dados, etc.
+# As namedtuples são imutáveis assim como as tuplas.
+# https://docs.python.org/3/library/collections.html#collections.namedtuple
+# https://docs.python.org/3/library/typing.html#typing.NamedTuple
+# https://brasilescola.uol.com.br/curiosidades/baralho.htm
+# from collections import namedtuple

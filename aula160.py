d3ab74e82e23f154bb974dc419ac8879ff731dbf
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-07-15 14:00:34 -0300

    update aula Namedtuple

--- aula160.py
+++ aula160.py
@@ -7,3 +7,22 @@
 # https://docs.python.org/3/library/typing.html#typing.NamedTuple
 # https://brasilescola.uol.com.br/curiosidades/baralho.htm
 # from collections import namedtuple
+from typing import NamedTuple
+
+class Carta(NamedTuple):
+    valor: str = 'VALOR'
+    naipe: str = 'NAIPE'
+
+
+# as_espada = Carta('A')
+
+# Carta = namedtuple('Carta', ['valor', 'naipe'], defaults=['', ''])
+as_espada = Carta('A', 'espadas')
+print(as_espada)
+print(as_espada.valor)
+print(as_espada._fields)
+
+print(as_espada._asdict())
+
+for valor in as_espada:
+    print(valor)

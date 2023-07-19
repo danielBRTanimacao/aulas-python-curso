d795a63588847044a2a50d72cb293ef9ab124e98
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-07-19 14:04:38 -0300

    aula datetime

---
+++ aula163.py
@@ -0,0 +1,4 @@
+# datetime.timedelta e dateutil.relativetimedelta (calculando datas)
+# Docs:
+# https://dateutil.readthedocs.io/en/stable/relativedelta.html
+# https://docs.python.org/3/library/datetime.html#timedelta-objects

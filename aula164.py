8355ba3ef31a48696091c19b567e05dea80e6c85
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-07-25 19:47:08 -0300

    aula datetime

--- aula164.py
+++ aula164.py
@@ -1,3 +1,8 @@
 # Formatando datas do datetime
 # datetime.strftime('DATA', 'FORMATO')
 # https://docs.python.org/3/library/datetime.html
+
+from datetime import datetime
+
+# data = datetime(2022, 12, 13, 7, 59, 23)
+data = datetime.strptime('2022-12-13 07:59:23', '%Y-%m-%d %H:%M:%S')

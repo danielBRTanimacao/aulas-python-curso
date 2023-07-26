46b06378edf2af42aeafba31e42d63fe6418b956
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-07-26 13:37:57 -0300

    aula datetime

--- aula164.py
+++ aula164.py
@@ -5,4 +5,5 @@
 from datetime import datetime
 
 # data = datetime(2022, 12, 13, 7, 59, 23)
-data = datetime.strptime('2022-12-13 07:59:23', '%Y-%m-%d %H:%M:%S')
+data = datetime.strptime('2023-07-26 13:34:00', '%Y-%m-%d %H:%M:%S')
+print(data)

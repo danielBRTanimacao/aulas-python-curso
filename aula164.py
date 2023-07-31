6602320d37c168642ce76b90f7c2517f537480d5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-07-31 13:24:08 -0300

    update aula datetime

--- aula164.py
+++ aula164.py
@@ -4,6 +4,10 @@
 
 from datetime import datetime
 
+format_ = '%d/%m/%Y'
 # data = datetime(2022, 12, 13, 7, 59, 23)
-data = datetime.strptime('2023-07-26 13:34:00', '%Y-%m-%d %H:%M:%S')
-print(data)
+data = datetime.strptime('2023-07-26 13:34:07', '%Y-%m-%d %H:%M:%S')
+print(data.strftime(format_))
+print(data.strftime('%Y'), data.year)
+print(data.strftime('%M'), data.minute)
+print(data.strftime('%S'), data.second)

86e52b3ec188fd0f8398cae7d4a95827db67dc0c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-03 13:02:00 -0300

    aula calendar

---
+++ aula166.py
@@ -0,0 +1,10 @@
+# Usando calendar para calendários e datas
+# https://docs.python.org/3/library/calendar.html
+# calendar é usado para coisas genéricas de calendários e datas.
+# Com calendar, você pode saber coisas como:
+# - Qual o último dia do mês (ex.: monthrange)
+# - Qual o nome e número do dia de determinada data (ex.: weekday)
+# - Criar um calendário em si (ex.: monthcalendar)
+# - Trabalhar com coisas específicas de calendários (ex.: calendar, month)
+# Por padrão dia da semana começa em 0 até 6
+# 0 = segunda-feira | 6 = domingo

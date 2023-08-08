8f6eba4616626f91e9cfdd855dfb1294c6798ec3
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-08 15:30:17 -0300

    update module calendar

--- aula166.py
+++ aula166.py
@@ -8,3 +8,11 @@
 # - Trabalhar com coisas específicas de calendários (ex.: calendar, month)
 # Por padrão dia da semana começa em 0 até 6
 # 0 = segunda-feira | 6 = domingo
+import calendar
+
+# print(calendar.calendar(2022))
+# print(calendar.month(2022, 12))
+# numero_primeiro_dia, ultimo_dia = calendar.monthrange(2022, 12)
+# print(list(enumerate(calendar.day_name)))
+# print(calendar.day_name[numero_primeiro_dia])
+# print(calendar.day_name[calendar.weekday(2022, 12, ultimo_dia)])

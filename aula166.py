4dfa772a439ccb7baf69d0bfb2f2bf92304a1f1e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-09 13:26:03 -0300

    update aula calendar

--- aula166.py
+++ aula166.py
@@ -11,8 +11,10 @@
 import calendar
 
 # print(calendar.calendar(2022))
-# print(calendar.month(2022, 12))
-# numero_primeiro_dia, ultimo_dia = calendar.monthrange(2022, 12)
-# print(list(enumerate(calendar.day_name)))
+# print(calendar.month(2022, 5))
+# print(calendar.monthrange(2023, 1))
+# for i, day in list(enumerate(calendar.day_name)):
+    # print(i, day)
+# numero_primeiro_dia , ultimo_dia = calendar.monthrange(2023, 8)
 # print(calendar.day_name[numero_primeiro_dia])
-# print(calendar.day_name[calendar.weekday(2022, 12, ultimo_dia)])
+# print(calendar.day_name[calendar.weekday(2023, 8, ultimo_dia)])

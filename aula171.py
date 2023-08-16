15ea70c82316f60c909902d0f0759d75b7abdced
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-16 14:53:16 -0300

    update module os.walk

---
+++ aula171.py
@@ -0,0 +1,5 @@
+# os.walk para navegar de caminhos de forma recursiva
+# os.walk é uma função que permite percorrer uma estrutura de diretórios de
+# maneira recursiva. Ela gera uma sequência de tuplas, onde cada tupla possui
+# três elementos: o diretório atual (root), uma lista de subdiretórios (dirs)
+# e uma lista dos arquivos do diretório atual (files).

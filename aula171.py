b5118afd4d45f38d6650163ab227ae8d0961c979
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-23 19:34:15 -0300

    update aula os.walk

--- aula171.py
+++ aula171.py
@@ -5,3 +5,20 @@
 # e uma lista dos arquivos do diretório atual (files).
 
 import os
+from itertools import count
+
+caminho = os.path.join('/Users', 'daniel', 'Desktop', 'EXEMPLO')
+counter = count()
+
+for root, dirs, files in os.walk(caminho):
+    the_counter = next(counter)
+    print(the_counter, 'Pasta atual', root)
+
+    for dir_ in dirs:
+        print('  ', the_counter, 'Dir:', dir_)
+
+    for file_ in files:
+        caminho_completo_arquivo = os.path.join(root, file_)
+        print('  ', the_counter, 'FILE:', caminho_completo_arquivo)
+        # NÃO FAÇA ISSO (VAI APAGAR TUDO DA PASTA)
+        # os.unlink(caminho_completo_arquivo)

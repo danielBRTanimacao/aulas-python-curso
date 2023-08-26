252a6d677cd7ba1911cd2dc3b03154c3b792fdbd
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-26 13:12:23 -0300

    module os.path.getsize e os.stat

--- aula172.py
+++ aula172.py
@@ -1,3 +1,37 @@
 # os.path.getsize e os.stat para dados dos arquivos (tamanho em bytes)
 
 import math
+import os
+from itertools import count
+
+
+def formata_tamanho(tamanho_em_bytes: int, base: int = 1000) -> str:
+    """Formata um tamanho, de bytes para o tamanho apropriado"""
+
+    # Original:
+    # https://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
+
+    # Se o tamanho for menor ou igual a 0, 0B.
+    if tamanho_em_bytes <= 0:
+        return "0B"
+
+    # Tupla com os tamanhos
+    #                      0    1     2     3     4     5
+    abreviacao_tamanhos = "B", "KB", "MB", "GB", "TB", "PB"
+    # Logaritmo -> https://brasilescola.uol.com.br/matematica/logaritmo.htm
+    # math.log vai retornar o logaritmo do tamanho_em_bytes
+    # com a base (1000 por padrão), isso deve bater
+    # com o nosso índice na abreviação dos tamanhos
+    indice_abreviacao_tamanhos = int(math.log(tamanho_em_bytes, base))
+    # Por quanto nosso tamanho deve ser dividido para
+    # gerar o tamanho correto.
+    potencia = base ** indice_abreviacao_tamanhos
+    # Nosso tamanho final
+    tamanho_final = tamanho_em_bytes / potencia
+    # A abreviação que queremos
+    abreviacao_tamanho = abreviacao_tamanhos[indice_abreviacao_tamanhos]
+    return f'{tamanho_final:.2f} {abreviacao_tamanho}'
+
+print(formata_tamanho(1000))
+print(formata_tamanho(1_000_000))
+print(formata_tamanho(1_000_000_000))

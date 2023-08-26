25549aaf7315af371579d77f542a77ce0ea9159b
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-08-26 13:31:56 -0300

    update module os.path.getsize e os.stat

--- aula172.py
+++ aula172.py
@@ -32,6 +32,26 @@ def formata_tamanho(tamanho_em_bytes: int, base: int = 1000) -> str:
     abreviacao_tamanho = abreviacao_tamanhos[indice_abreviacao_tamanhos]
     return f'{tamanho_final:.2f} {abreviacao_tamanho}'
 
-print(formata_tamanho(1000))
-print(formata_tamanho(1_000_000))
-print(formata_tamanho(1_000_000_000))
+caminho = os.path.join(
+    'C:/', 'Users', 'danie', 'OneDrive', 'Área de Trabalho', 
+    'vs estudo', 'testes_projetos_python', 'EXEMPLOS AULA 170'
+)
+counter = count()
+
+for root, dirs, files in os.walk(caminho):
+    the_counter = next(counter)
+    print(the_counter, 'Pasta atual', root)
+
+    for dir_ in dirs:
+        print(' ', the_counter, 'Dir:', dir_)
+    
+    for file_ in files:
+        caminho_completo_arquivo = os.path.join(root, file_)
+        stats = os.stat(caminho_completo_arquivo)
+        tamanho = stats.st_size
+        print(' ', the_counter, 'FILE:', file_, formata_tamanho(tamanho))
+
+
+# print(formata_tamanho(1000))
+# print(formata_tamanho(1_000_000))
+# print(formata_tamanho(1_000_000_000))

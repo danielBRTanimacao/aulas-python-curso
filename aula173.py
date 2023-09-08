e792f2e34bee778180c39f007fe48f0c7c2e379d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-09-08 17:33:38 -0300

    aula shutil

--- aula173.py
+++ aula173.py
@@ -9,3 +9,19 @@ HOME = os.path.expanduser('~')
 DESKTOP = os.path.join(HOME, 'Desktop')
 PASTA_ORIGINAL = os.path.join(DESKTOP, 'EXEMPLO')
 NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
+
+os.makedirs(NOVA_PASTA, exist_ok=True)
+
+for root, dirs, files in os.walk(PASTA_ORIGINAL):
+    for dir_ in dirs:
+        caminnho_novo_diretorio = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+        )
+        os.makedirs(caminnho_novo_diretorio, exist_ok=True)
+
+    for file in files:
+        caminho_arquivo = os.path.join(root, file)
+        caminnho_novo_arquivo = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+        )
+        shutil.copy(caminho_arquivo, caminnho_novo_arquivo)

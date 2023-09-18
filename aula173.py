85057c2678fca265ac92f944b1a4d512896ed0aa
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-09-18 16:44:32 -0300

    update aula shutil

--- aula173.py
+++ aula173.py
@@ -4,9 +4,3 @@
 
 import os
 import shutil
-
-HOME = os.path.expanduser('~')
-DESKTOP = os.path.join(HOME, 'Desktop')
-PASTA_ORIGINAL = os.path.join(DESKTOP, 'EXEMPLO')
-NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
-

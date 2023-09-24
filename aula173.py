be390e00dc42ffc52ef624f9ec27c1239e081109
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-09-24 17:35:25 -0300

    aula shutil

--- aula173.py
+++ aula173.py
@@ -2,4 +2,14 @@
 # Vamos copiar arquivos de uma pasta para outra.
 # Copiar -> shutil.copy
 
+import os
 import shutil
+
+HOME = os.path.expanduser('~')
+PATH_DOWNLOAD = os.path.join(HOME, 'Downloads')
+ORIGINAL_FOLDER = os.path.join(PATH_DOWNLOAD, 'exemplo_teste_aula')
+NEW_FOLDER = os.path.join(PATH_DOWNLOAD, 'exemplo02')
+
+for root, dirs, files in os.walk(ORIGINAL_FOLDER):
+    for file in files:
+        print(file)

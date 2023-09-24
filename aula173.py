0f0fc4a3029c7c26be616deb9f030cce660ca34a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-09-24 17:52:40 -0300

    final update aula shutil

--- aula173.py
+++ aula173.py
@@ -5,11 +5,22 @@
 import os
 import shutil
 
-HOME = os.path.expanduser('~')
+HOME = os.path.expanduser('~') #pega home do usuario
 PATH_DOWNLOAD = os.path.join(HOME, 'Downloads')
 ORIGINAL_FOLDER = os.path.join(PATH_DOWNLOAD, 'exemplo_teste_aula')
 NEW_FOLDER = os.path.join(PATH_DOWNLOAD, 'exemplo02')
 
+os.makedirs(NEW_FOLDER, exist_ok=True)
+
 for root, dirs, files in os.walk(ORIGINAL_FOLDER):
+    for dir_ in dirs: # cria as subpastas
+        new_path_directory = os.path.join(root.replace(
+            ORIGINAL_FOLDER, NEW_FOLDER), dir_
+        )
+        os.makedirs(new_path_directory, exist_ok=True)
+
     for file in files:
-        print(file)
+        path_file = os.path.join(root, file)
+        new_path_file = os.path.join(root.replace(ORIGINAL_FOLDER, NEW_FOLDER), file)
+
+        shutil.copy(path_file, new_path_file)

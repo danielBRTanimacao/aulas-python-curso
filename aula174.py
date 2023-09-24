aba2b67331b343c0fdba6f983de2e2aa377869b4
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-09-24 17:55:11 -0300

    aula shutil copiando e apagando

---
+++ aula174.py
@@ -0,0 +1,7 @@
+# os + shutil - Apagando, copiando, movendo e renomeando pastas com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+# Copiar Árvore recursivamente -> shutil.copytree
+# Apagar Árvore recursivamente -> shutil.rmtree
+# Apagar arquivos -> os.unlink
+# Renomear/Mover -> shutil.move ou os.rename

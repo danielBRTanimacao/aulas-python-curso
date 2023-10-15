bd4cb3f8365c2715e926b4cea9d25248703f9205
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-10-15 18:53:13 -0300

    aula json

---
+++ aula176.py
@@ -0,0 +1,24 @@
+# json.dumps e json.loads com strings + typing.TypedDict
+# Ao converter de Python para JSON:
+# Python        JSON
+# dict          object
+# list, tuple   array
+# str           string
+# int, float    number
+# True          true
+# False         false
+# None          null
+
+string_json = '''
+{
+    "title": "O Senhor dos Anéis: A Sociedade do Anel",
+    "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+    "is_movie": true,
+    "imdb_rating": 8.8,
+    "year": 2001,
+    "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+    "budget": null
+}
+'''
+
+print(string_json)

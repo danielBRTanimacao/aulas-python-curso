668bfce948411cc5bde85d8e8a3b169c55e65b5d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-10-19 15:25:21 -0300

    aula json dump load com arquivo

--- aula177.py
+++ aula177.py
@@ -4,13 +4,13 @@ import os
 
 FILE_NAME = 'aula177.json'
 PATH_ABSOLUT_FILE = os.path.abspath(
-    os.path.join(
+    os.path.join( 
         os.path.dirname(__file__),
         FILE_NAME
     )
 )
 
-filme = {
+movie = {
     'title': 'O Senhor dos Anéis: A Sociedade do Anel',
     'original_title': 'The Lord of the Rings: The Fellowship of the Ring',
 
@@ -20,3 +20,10 @@ filme = {
     'characters': ['Frodo', 'Sam', 'Gandalf', 'Legolas', 'Boromir'],
     'budget': None
 }
+
+with open(PATH_ABSOLUT_FILE, 'w') as file:
+    json.dump(movie, file, ensure_ascii=False, indent=2)
+
+with open(PATH_ABSOLUT_FILE, 'r') as file:
+    movie_do_json = json.load(file)
+    print(movie_do_json)

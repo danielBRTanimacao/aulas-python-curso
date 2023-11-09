4ec1757eb484d7ffc3dd8e363c3d2b100dd1aab3
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-09 18:50:35 -0300

    aula csv write

---
+++ aula180.py
@@ -0,0 +1,13 @@
+# csv.writer e csv.DictWriter para escrever em CSV
+# csv.reader lê o CSV em formato de lista
+# csv.DictReader lê o CSV em formato de dicionário
+import csv
+from pathlib import Path
+
+CAMINHO_CSV = Path(__file__).parent / 'aula180.csv'
+
+lista_clientes = [
+    {'Nome': 'Luiz Otávio', 'Endereço': 'Av 1, 22'},
+    {'Nome': 'João Silva', 'Endereço': 'R. 2, "1"'},
+    {'Nome': 'Maria Sol', 'Endereço': 'Av B, 3A'},
+]

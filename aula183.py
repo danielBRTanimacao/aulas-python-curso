b4273833bd73773ad0a5be6c7d92c680c9c7c079
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-14 13:14:11 -0300

    string templates

---
+++ aula183.py
@@ -0,0 +1,6 @@
+# string.Template para substituir variáveis em textos
+# doc: https://docs.python.org/3/library/string.html#template-strings
+# Métodos:
+# substitute: substitui mas gera erros se faltar chaves
+# safe_substitute: substitui sem gerar erros
+# Você também pode trocar o delimitador e outras coisas criando uma subclasse

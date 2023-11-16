3b35a02533110ae02f01e60afef6a27981038a37
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-16 13:56:09 -0300

    modulo sys

---
+++ aula187.py
@@ -0,0 +1,18 @@
+# sys.argv - Executando arquivos com argumentos no sistema
+# Fonte = Fira Code
+import sys
+
+# print(sys.argv)
+
+argumentos = sys.argv
+qtd_argumentos = len(argumentos)
+
+if qtd_argumentos <= 1:
+    print('Você não passou argumentos')
+else:
+    try:
+        print(f'Você passou os argumentos {argumentos[1:]}')
+        print(f'Faça alguma coisa com {argumentos[1]}')
+        print(f'Faça outra coisa com {argumentos[2]}')
+    except IndexError:
+        print('Faltam argumentos')

0f791827ca4baff05f6e97221ec3b2fa64255128
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-25 19:50:22 -0300

    aula requests

--- aula191.py
+++ aula191.py
@@ -1,3 +1,14 @@
 # requests para requisições HTTP
 # Tutorial -> https://youtu.be/Qd8JT0bnJGs
 import requests
+
+# http:// -> 80
+# https:// -> 443
+
+url = 'http://127.0.0.1:5500/aula190_site/index.html'
+response = requests.get(url) # resposta
+
+print(response.status_code)
+# print(response.headers)
+# print(response.content)
+print(response.text)

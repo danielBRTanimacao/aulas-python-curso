f5813205240b78115018b3c5f0f5493e459310d5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-11-25 19:28:26 -0300

    aula requests

--- aula191.py
+++ aula191.py
@@ -1,8 +1,3 @@
 # requests para requisições HTTP
 # Tutorial -> https://youtu.be/Qd8JT0bnJGs
-import resquests
-
-# http:// -> 80
-# https:// -> 443
-url = 'http://localhost:3333/'
-response = requests.get(url)
+import requests

6d8863c6d2b9cf92de398066d1877aa4dcd48ac8
Daniel Tenório <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-05 20:20:42 -0300

    Update main.py

--- aula205/main.py
+++ aula205/main.py
@@ -5,11 +5,7 @@ ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
 
-connection = sqlite3.connect(DB_FILE)
-cursor = connection.cursor()
-
 # SQL
-cursor.execute()
 
 cursor.close()
 connection.close()

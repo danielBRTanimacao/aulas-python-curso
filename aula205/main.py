2ec85e05c7dd563e46231bced4917162a0037646
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-12 10:41:38 -0300

    upgrade aula sqlite

--- aula205/main.py
+++ aula205/main.py
@@ -4,11 +4,17 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-# SQL
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,' #primary key
+    ')'
+)
 
 cursor.close()
 connection.close()

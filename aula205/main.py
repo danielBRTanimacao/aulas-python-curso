fd0b587562eb812964b90e3d476d16cbf94dd16c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-17 11:45:24 -0300

    aula sqlite criando e adicionando colunas

--- aula205/main.py
+++ aula205/main.py
@@ -9,13 +9,31 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# CUIDADO: Fazendo DELETE sem WHERE
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+# cria a tabela
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
     'id INTEGER PRIMARY KEY AUTOINCREMENT,' #primary key
     'name TEXT,'
-    'wheight REAL'
+    'weight REAL'
     ')'
+)
+connection.commit()
+
+# Registrar valores
+# CUIDADO: com sql injection 
+connection.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight) '
+    'VALUES (NULL, "Daniel Batata", 103.2), (NULL, "Cururu Pidão", 73)'
 )
 connection.commit()
 

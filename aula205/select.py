fdf91db911517be157e6ca48d23f81d7292c6554
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-19 13:25:33 -0300

    Select do SQL com fetch do sqlite

---
+++ aula205/select.py
@@ -0,0 +1,58 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+# CUIDADO: Fazendo DELETE sem WHERE
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+# cria a tabela
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,' #primary key
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
+
+# Registrar valores
+# CUIDADO: com sql injection NÃO PRECISA MAIS DE CUIDADO
+sql = (
+    f'INSERT INTO {TABLE_NAME} (name, weight) '
+    'VALUES '
+    '(:nome, :peso)' # Agora ele sabe que os valores estão separados
+)
+# connection.execute(sql, ["Bico Seco", 1]) Execute, executa uma unica coisa e executemany varias coisas
+# connection.executemany(
+#     sql, (
+#         ("Bico Seco", 1), ("Cururu Pidão", 4)
+#     )
+# ) 
+connection.execute(sql, {'nome': "Silas Mahoraga", 'peso': 10})
+connection.executemany(
+    sql, (
+        {'nome': "Silas Mahoraga", 'peso': 10},
+        {'nome': "Akasa", 'peso': 12},
+        {'nome': "Pidão", 'peso': 2},
+        {'nome': "Cleitin do Grau", 'peso': 6}
+    )
+)
+print(sql)
+connection.commit()
+
+cursor.close()
+connection.close()

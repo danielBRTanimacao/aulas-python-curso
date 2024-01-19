0d1c0a7aeb44ac0114e754edf5608248012246a7
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-01-19 14:55:47 -0300

    Aula Django Erro 404 e config post.html

--- aula207/blog/views.py
+++ aula207/blog/views.py
@@ -1,5 +1,8 @@
+from typing import Any
+
 from django.shortcuts import render
 from blog.data import posts
+from django.http import HttpRequest, Http404
 
 
 def blog(request):
@@ -13,14 +16,25 @@ def blog(request):
 
     return render(request, 'blog/index.html', context)
 
-def post(request, idPosts):
+def post(request: HttpRequest, idPosts: int):
+
+    found_post: dict[str, Any] | None = None
+
+    for post in posts:
+        if post['id'] == idPosts:
+            found_post = post
+            break
+    
+    if found_post is None:
+        raise Http404('Post não existe mermão')
+
     context = {
         'text': f'Aqui está o post número {idPosts}',
-        'title': 'Posts - ',
-        'posts': posts
+        'title': found_post['title'] + ' - ',
+        'varpost': found_post
     }
 
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/post.html', context)
 
 def exemplo(request):
     print('blog')

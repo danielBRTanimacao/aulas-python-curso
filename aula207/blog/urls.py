0303b9e60ca3df30bbbb6c6b12b1845fc25e416e
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-01-19 13:58:46 -0300

    Aula Django usando a mesma URL estatica e dinamica

--- aula207/blog/urls.py
+++ aula207/blog/urls.py
@@ -5,6 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog, name='home'),
-    path('post/<int:idPosts>', views.post, name='post'),
+    path('<int:idPosts>/', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
